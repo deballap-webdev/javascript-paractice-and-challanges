92f28b236e628bd19a42028a5b0d61f119ca1ab8
deballap-webdev <debbieallap@gmail.com>
2026-07-03 20:52:03 +0100

    refactored even-odd splitter with specialized functions

--- main-challenge/level-1/even-odd-splitter/main.js
+++ main-challenge/level-1/even-odd-splitter/main.js
@@ -1,3 +1,4 @@
+"use strict";
 const myIntegers = [1, 2, 8, 7, 4, 9, 3, 4, 5, 6, 11, 13, 12];
 
 const evenOddSplitter = (intarray) => {
@@ -7,17 +8,12 @@ const evenOddSplitter = (intarray) => {
       "Your array includes floating (decimal) numbers, please enter only integers (whole numbers) and try again",
     );
   } else {
-    let evenNumbers = intarray.filter((integer) => {
-      return integer % 2 === 0;
-    });
+    let even = getEvenNumbersFromArray(intarray);
+    let odd = getOddNumbersFromArray(intarray);
 
-    let oddNumbers = intarray.filter((integer) => {
-      return integer % 2 !== 0;
-    });
-
-    evenNumbers = evenNumbers.join(", ");
-    oddNumbers = oddNumbers.join(" ,");
-    console.log(`even numbers: ${evenNumbers}\nOdd Numbers: ${oddNumbers}`);
+    even = formatArray(even);
+    odd = formatArray(odd);
+    logEvenAndOddNumbers(even, odd);
   }
 };
 
@@ -25,6 +21,28 @@ const checkForValidArray = (array) => {
   return array.every((item) => {
     return Number.isInteger(item);
   });
+};
+
+const getEvenNumbersFromArray = (array) => {
+  const evenNumbers = array.filter((integer) => {
+    return integer % 2 === 0;
+  });
+  return evenNumbers;
+};
+
+const getOddNumbersFromArray = (array) => {
+  const oddNumbers = array.filter((integer) => {
+    return integer % 2 !== 0;
+  });
+  return oddNumbers;
+};
+
+const formatArray = (array) => {
+  return array.join(", ");
+};
+
+const logEvenAndOddNumbers = (even, odd) => {
+  console.log(`even numbers: ${even}\nOdd Numbers: ${odd}`);
 };
 
 evenOddSplitter(myIntegers);
