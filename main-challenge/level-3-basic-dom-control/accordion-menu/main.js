95f71a75695fbce60f9eaed10469eb2377dd3dba
deballap-webdev <debbieallap@gmail.com>
2026-07-14 12:44:10 +0100

    make accordion menu have only one active item at a time using state-tracking method

--- main-challenge/level-3-basic-dom-control/accordion-menu/main.js
+++ main-challenge/level-3-basic-dom-control/accordion-menu/main.js
@@ -1,3 +1,4 @@
+"use strict";
 document.addEventListener("readystatechange", (event) => {
   if (event.target.readyState === "complete") {
     initApp();
@@ -6,22 +7,47 @@ document.addEventListener("readystatechange", (event) => {
 
 const initApp = () => {
   const questionButtons = document.querySelectorAll(".question-list__button");
-  /* const questionButton = document.querySelector("#question-list__button-1");
 
-  console.log(questionButton);
- */
+  let holdAnswer = null;
+  let holdArrow = null;
+  const activeAccordionStateTracker = (arrow, answer) => {
+    if (arrow && answer) {
+      if (
+        arrow.classList.contains("activeArrow") &&
+        answer.classList.contains("displayBlock")
+      ) {
+        arrow.classList.remove("activeArrow");
+        answer.classList.remove("displayBlock");
+        answer.classList.add("displayNone");
+      } else {
+        console.log(`something is wrong!`);
+      }
+    }
+  };
 
   questionButtons.forEach((questionButton) => {
-    console.log(questionButton.parentElement);
-
     questionButton.addEventListener("click", (event) => {
       const answer = questionButton.parentElement.querySelector(
         ".answer-container .answer",
       );
       const arrow = questionButton.querySelector(".arrow");
+      if (holdAnswer !== answer && holdArrow !== arrow) {
+        activeAccordionStateTracker(holdArrow, holdAnswer);
+      } else {
+        holdAnswer = null;
+        holdArrow = null;
+      }
       answer.classList.toggle("displayBlock");
       answer.classList.toggle("displayNone");
       arrow.classList.toggle("activeArrow");
+
+      console.log(
+        arrow.classList.contains("activeArrow") &&
+          answer.classList.contains("displayBlock"),
+      );
+
+      holdAnswer = answer;
+      holdArrow = arrow;
     });
   });
 };
