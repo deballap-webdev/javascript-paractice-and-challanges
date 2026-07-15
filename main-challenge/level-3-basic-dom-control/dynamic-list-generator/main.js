87771f5631b01bb63147833a3be23b8a2ef5683c
deballap-webdev <debbieallap@gmail.com>
2026-07-15 22:31:06 +0100

    toggle list section display and option buttons display

--- main-challenge/level-3-basic-dom-control/dynamic-list-generator/main.js
+++ main-challenge/level-3-basic-dom-control/dynamic-list-generator/main.js
@@ -5,10 +5,19 @@ document.addEventListener("readystatechange", (event) => {
 });
 
 const initApp = () => {
+  let currentOption = null;
   const addListItemForm = document.querySelector("#add-list-item-form");
   const addListItemInput = addListItemForm.querySelector("textarea");
   const listSection = document.querySelector("#list-section");
   const list = listSection.querySelector(".list");
+  const listSectionDisplay = () => {
+    if (!list.hasChildNodes()) {
+      listSection.style.display = "none";
+    } else {
+      listSection.style.display = "";
+    }
+  };
+  listSectionDisplay();
 
   addListItemInput.addEventListener("input", (event) => {
     event.target.style.height = "auto";
@@ -20,21 +29,36 @@ const initApp = () => {
     addListItemInput.style.height = "auto";
     const newListItemValue = addListItemInput.value;
     const newListItem = document.createElement("li");
-    const newListItemButton = document.createElement("button");
-    newListItemButton.textContent = newListItemValue;
-    newListItem.append(newListItemButton);
+
+    newListItem.innerHTML = `<button type="button">${newListItemValue}</button><span class="options displayNone"><button>remove</button> <button>Modify</button><button>Strike</button></span>`;
     list.append(newListItem);
     addListItemInput.value = "";
-    console.log(list.innerHTML);
-    const myList = localStorage("listStore", list.innerHTML);
+    handleListClick();
+
+    listSectionDisplay();
   });
 
-  [...list.children].forEach((listItem) => {
-    listItem.addEventListener("click", (event) => {
-      const currentOption = listItem.querySelector(".options");
-      if(currentOption.classList.contains("displayFlex"))
-      currentOption.classList.toggle("displayFlex");
-      currentOption.classList.toggle("displayNone");
+  const handleListClick = () => {
+    [...list.children].forEach((listItem) => {
+      listItem.addEventListener("click", (event) => {
+        const option = listItem.querySelector(".options");
+        if (currentOption !== null && currentOption !== option) {
+          if (currentOption.classList.contains("displayFlex")) {
+            currentOption.classList.remove("displayFlex");
+            currentOption.classList.add("displayNone");
+          }
+        } else {
+          currentOption = null;
+        }
+
+        option.classList.toggle("displayFlex");
+        option.classList.toggle("displayNone");
+
+        console.log(option.classList);
+        if (currentOption !== option) {
+          currentOption = option;
+        }
+      });
     });
-  });
+  };
 };
