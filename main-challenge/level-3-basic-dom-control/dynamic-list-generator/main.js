2973bab1819920f4780474f6ce20fc811d3a3f92
deballap-webdev <debbieallap@gmail.com>
2026-07-16 19:18:12 +0100

    add functionality to remove, strike/unstrike and modify option buttons

--- main-challenge/level-3-basic-dom-control/dynamic-list-generator/main.js
+++ main-challenge/level-3-basic-dom-control/dynamic-list-generator/main.js
@@ -1,3 +1,4 @@
+"use strict";
 document.addEventListener("readystatechange", (event) => {
   if (event.target.readyState === "complete") {
     initApp();
@@ -6,10 +7,17 @@ document.addEventListener("readystatechange", (event) => {
 
 const initApp = () => {
   let currentOption = null;
-  const addListItemForm = document.querySelector("#add-list-item-form");
+  let modifiedListItemValue = null;
+  let listItemToModify = null;
+
+  const taskManagementSection = document.querySelector("#task-management");
+  const addListItemForm = taskManagementSection.querySelector(
+    "#add-list-item-form",
+  );
   const addListItemInput = addListItemForm.querySelector("textarea");
-  const listSection = document.querySelector("#list-section");
+  const listSection = taskManagementSection.querySelector("#list-section");
   const list = listSection.querySelector(".list");
+  const optionGroup = [];
   const listSectionDisplay = () => {
     if (!list.hasChildNodes()) {
       listSection.style.display = "none";
@@ -19,10 +27,19 @@ const initApp = () => {
   };
   listSectionDisplay();
 
-  addListItemInput.addEventListener("input", (event) => {
-    event.target.style.height = "auto";
-    event.target.style.height = event.target.scrollHeight + "px";
-  });
+  const modifyListItemSection = document.querySelector("#modify-list-item");
+  const modifyListItemForm = modifyListItemSection.querySelector("form");
+  const modifyListItemInput =
+    modifyListItemForm.querySelector("#modify-list-input");
+
+  const elasticInputField = (inputField) => {
+    inputField.addEventListener("input", (event) => {
+      event.target.style.height = "auto";
+      event.target.style.height = event.target.scrollHeight + "px";
+    });
+  };
+
+  elasticInputField(addListItemForm);
 
   addListItemForm.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -30,35 +47,111 @@ const initApp = () => {
     const newListItemValue = addListItemInput.value;
     const newListItem = document.createElement("li");
 
-    newListItem.innerHTML = `<button type="button">${newListItemValue}</button><span class="options displayNone"><button>remove</button> <button>Modify</button><button>Strike</button></span>`;
+    newListItem.innerHTML = `<button class="strikeable" type="button">${newListItemValue}</button><span class="options displayNone"><button>remove</button> <button>modify</button><button>strike</button></span>`;
     list.append(newListItem);
     addListItemInput.value = "";
-    handleListClick();
-
     listSectionDisplay();
-  });
+    const option = newListItem.querySelector(".options");
+    optionGroup.push(option);
 
-  const handleListClick = () => {
-    [...list.children].forEach((listItem) => {
-      listItem.addEventListener("click", (event) => {
-        const option = listItem.querySelector(".options");
-        if (currentOption !== null && currentOption !== option) {
-          if (currentOption.classList.contains("displayFlex")) {
-            currentOption.classList.remove("displayFlex");
-            currentOption.classList.add("displayNone");
-          }
+    option.querySelectorAll("button").forEach((optionBtn) => {
+      optionBtn.addEventListener("click", (event) => {
+        if (optionBtn.textContent === "remove") {
+          optionBtn.closest("li").remove();
+          listSectionDisplay();
+        } else if (optionBtn.textContent === "strike") {
+          optionBtn
+            .closest("li")
+            .querySelector(".strikeable").style.textDecoration = "line-through";
+          optionBtn.textContent = "unstrike";
+        } else if (optionBtn.textContent === "unstrike") {
+          optionBtn
+            .closest("li")
+            .querySelector(".strikeable").style.textDecoration = "none";
+          optionBtn.textContent = "strike";
         } else {
-          currentOption = null;
-        }
+          taskManagementSection.classList.add("displayNone");
+          modifyListItemSection.classList.remove("displayNone");
 
-        option.classList.toggle("displayFlex");
-        option.classList.toggle("displayNone");
-
-        console.log(option.classList);
-        if (currentOption !== option) {
-          currentOption = option;
+          modifyListItemInput.value = optionBtn
+            .closest("li")
+            .querySelector(".strikeable").textContent;
+          listItemToModify = optionBtn
+            .closest("li")
+            .querySelector(".strikeable");
+          handleModifyOperation();
         }
       });
     });
+
+    newListItem.addEventListener("click", (event) => {
+      if (currentOption !== null && currentOption !== option) {
+        if (currentOption.classList.contains("displayFlex")) {
+          currentOption.classList.remove("displayFlex");
+          currentOption.classList.add("displayNone");
+        }
+      } else {
+        currentOption = null;
+      }
+
+      option.classList.toggle("displayFlex");
+      option.classList.toggle("displayNone");
+
+      if (currentOption !== option) {
+        currentOption = option;
+      }
+    });
+  });
+
+  const handleModifyOperation = () => {
+    elasticInputField(modifyListItemInput);
+    if (!modifyListItemForm.classList.contains("displayNone")) {
+      const confirmModifyButton =
+        modifyListItemForm.querySelector("#confirmModify");
+      const cancelModifyButton =
+        modifyListItemForm.querySelector("#cancelModify");
+      confirmModifyButton.classList.add("displayNone");
+
+      modifyListItemInput.addEventListener("input", (event) => {
+        if (
+          modifyListItemInput.value !== listItemToModify.textContent &&
+          modifyListItemInput.value.trim() !== ""
+        ) {
+          confirmModifyButton.classList.remove("displayNone");
+          modifiedListItemValue = modifyListItemInput.value;
+        } else {
+          if (!confirmModifyButton.classList.contains("displayNone")) {
+            confirmModifyButton.classList.add("displayNone");
+          }
+        }
+      });
+
+      confirmModifyButton.addEventListener("click", (event) => {
+        const isModifyConfirmed = confirm(
+          `are you sure you want to make these changes to your list\nOriginal List Item: ${listItemToModify.textContent}\nNew List Item: ${modifiedListItemValue}`,
+        );
+
+        if (isModifyConfirmed) {
+          modifyListItemSection.classList.add("displayNone");
+          taskManagementSection.classList.remove("displayNone");
+          listItemToModify.textContent = modifiedListItemValue;
+          listItemToModify = null;
+          modifiedListItemValue = null;
+        } else {
+          modifyListItemInput.value = modifiedListItemValue;
+        }
+      });
+
+      cancelModifyButton.addEventListener("click", (event) => {
+        listItemToModify = null;
+        modifiedListItemValue = null;
+        modifyListItemSection.classList.add("displayNone");
+        taskManagementSection.classList.remove("displayNone");
+      });
+    }
   };
+
+  modifyListItemForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+  });
 };
