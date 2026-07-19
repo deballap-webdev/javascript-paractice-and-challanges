ae958f09577862d5df92fafe198002e4f8adaa3f
deballap-webdev <debbieallap@gmail.com>
2026-07-19 22:36:07 +0100

    add localStorage

--- main-challenge/level-3-basic-dom-control/bookmark-keeper/main.js
+++ main-challenge/level-3-basic-dom-control/bookmark-keeper/main.js
@@ -6,6 +6,13 @@ document.addEventListener("readystatechange", (event) => {
   }
 });
 
+let id = 0;
+
+const generateId = () => {
+  id++;
+  return id;
+};
+
 const initApp = () => {
   const websiteNameInput = document.querySelector("#websiteName");
   const websiteURLInput = document.querySelector("#websiteURL");
@@ -30,21 +37,55 @@ const initApp = () => {
     parent.append(child);
   };
 
+  const formatURL = (URL) => {
+    const originalURL = URL.trim();
+    if (originalURL.startsWith("https://")) {
+      return originalURL;
+    } else {
+      return `https://${originalURL}`;
+    }
+  };
+
+  const websiteArray = [];
+
+  const render = () => {
+    websiteDetailsList.innerHTML = "";
+    websiteArray.forEach((bookmark) => {
+      const websiteDetailsListItem = document.createElement("li");
+      append(
+        websiteDetailsList,
+        websiteDetailsListItem,
+        `Name: ${bookmark.name} URL: `,
+      );
+      const websiteLink = document.createElement("a");
+      append(websiteDetailsListItem, websiteLink, `${bookmark.url}`);
+      websiteLink.href = `${bookmark.url}`;
+      websiteLink.target = "_blank";
+      websiteLink.rel = "noopener";
+    });
+
+    localStorage.setItem("bookmarks", JSON.stringify(websiteArray));
+  };
+
+  const pushSavedBookmarks = () => {
+    const savedBookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+    console.log(savedBookmarks);
+    if (savedBookmarks) {
+      websiteArray.push(...savedBookmarks);
+    }
+  };
+
+  pushSavedBookmarks();
+  render();
+
+  const pushToWebsiteArray = (id, name, url) => {
+    websiteArray.push({ id: id, name: name, url: url });
+  };
+
   const addBookmark = (websiteName, websiteURL, parent) => {
-    const websiteDetailsListItem = document.createElement("li");
-    const websiteLink = document.createElement("a");
-    websiteLink.href = `${websiteURL}`;
-    websiteLink.target = "_blank";
-    
-    append(
-      websiteDetailsList,
-      websiteDetailsListItem,
-      `Name: ${websiteName} URL: `,
-    );
-
-    append(websiteDetailsListItem, websiteLink, `${websiteURL}`);
-
-    console.log(websiteDetailsListItem);
+    const websiteId = generateId();
+    pushToWebsiteArray(websiteId, `${websiteName}`, `${formatURL(websiteURL)}`);
+    render();
   };
 
   websiteDetailsForm.addEventListener("submit", (event) => {
